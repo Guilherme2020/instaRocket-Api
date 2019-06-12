0c573d33c481234518de3f195db8a3076522abe2
Guilherme Rodrigues <guilherme@ideiasti.com.br>
2019-06-12 09:58:02 -0300

    configuracao final do multer e socket io

--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -1,15 +1,55 @@
 const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
     
     async index(req,res){
 
-        return; 
+        const post = await Post.find().sort('-createdAt')
+
+        return res.json(post) 
     
     },
 
     async store(req,res){
-        res.json({ ok : true })   
+
+        console.log(req.body)
+
+        const {author,place,description, hashtags} = req.body
+        
+        const {filename: image} = req.file;
+
+        const [name] = image.split('.')
+        const fileName = `${name}.jpg`
+
+
+
+        // return res.json(req.file)
+
+        await sharp(req.file.path)
+                .resize(500)
+                .jpeg({ quality:70 })
+                .toFile( 
+                    path.resolve(req.file.destination,'resized',image)
+                )
+
+        fs.unlinkSync(req.file.path);
+
+         
+        const post = await Post.create({
+
+            author,
+            place,
+            description,
+            hashtags,
+            image: fileName
+
+        })
+        req.io.emit('post', post)
+        
+        return res.json(post)   
     }
 
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server)
+
+
 mongoose.connect('mongodb+srv://semana:semana@instarocket-ddsod.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser:true  
 })
 
+
+app.use((req,res,next) => {
+
+    req.io = io;
+
+    next()
+})
+
+app.use(cors())
+
+app.use('./files', express.static(path.resolve(__dirname,'..','uploads','resized')));
+
 app.use(require('./routes'))
 
 
-app.listen(3333);
+server.listen(3333);
 
 
 
